3559e39b16d6a9e3ccb46896483622b6d279b7ab
牛强 <jony4xx@gmail.com>
2021-04-19 18:54:59 +0800

    regen xml mappinged struct

--- pkg/bpmn-parse/schema/gen.sh
+++ pkg/bpmn-parse/schema/gen.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
- xsdgen -o BPMN20_xsdgen.go -pkg schema -f -vv BPMN20.xsd
+ xsdgen -o bpmn/BPMN20_xsdgen.go -pkg schema -f -vv BPMN20.xsd
  xsdgen -o bpmndi/BPMNDI_xsdgen.go -pkg schema -f -vv BPMNDI.xsd
  xsdgen -o dc/DC_xsdgen.go -pkg schema -f -vv DC.xsd
  xsdgen -o di/DI_xsdgen.go -pkg schema -f -vv DI.xsd
- xsdgen -o Semantic_xsdgen.go -pkg schema -f -vv Semantic.xsd
+ xsdgen -o semantic/Semantic_xsdgen.go -pkg schema -f -vv Semantic.xsd
