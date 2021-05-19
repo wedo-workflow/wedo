8477b723b8ef0c22d1ffe6a050d15f733de5b643
jony4 <jony4@users.noreply.github.com>
2021-05-19 03:38:15 +0000

    deploy: 9ecbb356d3f9c42440ad0ec02fc728efea6631eb

--- cmd/apiserver/app/server.go
+++
@@ -1,7 +0,0 @@
-package app
-
-import "fmt"
-
-func HelloAPIServer() {
-	fmt.Println("hello apiserver!")
-}
--- pkg/store/metadata/metadata.go
+++
@@ -1,6 +0,0 @@
-package metadata
-
-// Metadata contains a state store specific set of metadata properties
-type Metadata struct {
-	Properties map[string]string `json:"properties"`
-}
