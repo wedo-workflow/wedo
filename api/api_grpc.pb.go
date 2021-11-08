486a2c6f01a9a1297e9215ddfdd8329c58431e81
Jony4 <jony4xx@gmail.com>
2021-11-08 15:28:11 +0800

    update api annotation

--- api/api_grpc.pb.go
+++ api/api_grpc.pb.go
@@ -22,25 +22,32 @@ type WedoServiceClient interface {
 	DeploymentGet(ctx context.Context, in *DeploymentRequest, opts ...grpc.CallOption) (*DeploymentResponse, error)
 	// Creates a deployment.
 	DeploymentCreate(ctx context.Context, in *DeploymentCreateRequest, opts ...grpc.CallOption) (*DeploymentCreateResponse, error)
+	// Updates a deployment.
 	DeploymentList(ctx context.Context, in *DeploymentListRequest, opts ...grpc.CallOption) (*DeploymentListResponse, error)
+	// Delete a deployment.
 	DeploymentDelete(ctx context.Context, in *DeploymentDeleteRequest, opts ...grpc.CallOption) (*DeploymentDeleteResponse, error)
-	// User
+	// Create a User.
 	UserCreate(ctx context.Context, in *UserCreateRequest, opts ...grpc.CallOption) (*UserCreateResponse, error)
+	// Get a User.
 	UserGet(ctx context.Context, in *UserRequest, opts ...grpc.CallOption) (*UserResponse, error)
+	// Delete a User.
 	UserDelete(ctx context.Context, in *UserDeleteRequest, opts ...grpc.CallOption) (*UserDeleteResponse, error)
+	// List all Users.
 	UserList(ctx context.Context, in *UserListRequest, opts ...grpc.CallOption) (*UserListResponse, error)
 	UserUpdate(ctx context.Context, in *UserUpdateRequest, opts ...grpc.CallOption) (*UserUpdateResponse, error)
 	UserListCount(ctx context.Context, in *UserListCountRequest, opts ...grpc.CallOption) (*UserListCountResponse, error)
-	// Namespace
+	// Create a Namespace.
 	NamespaceCreate(ctx context.Context, in *NamespaceCreateRequest, opts ...grpc.CallOption) (*NamespaceCreateResponse, error)
+	// Get a Namespace.
 	NamespaceGet(ctx context.Context, in *NamespaceRequest, opts ...grpc.CallOption) (*NamespaceResponse, error)
+	// Delete a Namespace.
 	NamespaceDelete(ctx context.Context, in *NamespaceDeleteRequest, opts ...grpc.CallOption) (*NamespaceDeleteResponse, error)
+	// List all Namespaces.
 	NamespaceList(ctx context.Context, in *NamespaceListRequest, opts ...grpc.CallOption) (*NamespaceListResponse, error)
+	// Count all Namespaces.
 	NamespaceListCount(ctx context.Context, in *NamespaceListCountRequest, opts ...grpc.CallOption) (*NamespaceListCountResponse, error)
-	// Process Definition
 	ProcessDefinitionStart(ctx context.Context, in *ProcessDefinitionCreateRequest, opts ...grpc.CallOption) (*ProcessDefinitionCreateResponse, error)
 	ProcessDefinitionGet(ctx context.Context, in *ProcessDefinitionRequest, opts ...grpc.CallOption) (*ProcessDefinitionResponse, error)
-	// Task
 	TaskCreate(ctx context.Context, in *TaskCreateRequest, opts ...grpc.CallOption) (*TaskCreateResponse, error)
 	TaskGet(ctx context.Context, in *TaskRequest, opts ...grpc.CallOption) (*TaskResponse, error)
 	TaskDelete(ctx context.Context, in *TaskDeleteRequest, opts ...grpc.CallOption) (*TaskDeleteResponse, error)
@@ -252,25 +259,32 @@ type WedoServiceServer interface {
 	DeploymentGet(context.Context, *DeploymentRequest) (*DeploymentResponse, error)
 	// Creates a deployment.
 	DeploymentCreate(context.Context, *DeploymentCreateRequest) (*DeploymentCreateResponse, error)
+	// Updates a deployment.
 	DeploymentList(context.Context, *DeploymentListRequest) (*DeploymentListResponse, error)
+	// Delete a deployment.
 	DeploymentDelete(context.Context, *DeploymentDeleteRequest) (*DeploymentDeleteResponse, error)
-	// User
+	// Create a User.
 	UserCreate(context.Context, *UserCreateRequest) (*UserCreateResponse, error)
+	// Get a User.
 	UserGet(context.Context, *UserRequest) (*UserResponse, error)
+	// Delete a User.
 	UserDelete(context.Context, *UserDeleteRequest) (*UserDeleteResponse, error)
+	// List all Users.
 	UserList(context.Context, *UserListRequest) (*UserListResponse, error)
 	UserUpdate(context.Context, *UserUpdateRequest) (*UserUpdateResponse, error)
 	UserListCount(context.Context, *UserListCountRequest) (*UserListCountResponse, error)
-	// Namespace
+	// Create a Namespace.
 	NamespaceCreate(context.Context, *NamespaceCreateRequest) (*NamespaceCreateResponse, error)
+	// Get a Namespace.
 	NamespaceGet(context.Context, *NamespaceRequest) (*NamespaceResponse, error)
+	// Delete a Namespace.
 	NamespaceDelete(context.Context, *NamespaceDeleteRequest) (*NamespaceDeleteResponse, error)
+	// List all Namespaces.
 	NamespaceList(context.Context, *NamespaceListRequest) (*NamespaceListResponse, error)
+	// Count all Namespaces.
 	NamespaceListCount(context.Context, *NamespaceListCountRequest) (*NamespaceListCountResponse, error)
-	// Process Definition
 	ProcessDefinitionStart(context.Context, *ProcessDefinitionCreateRequest) (*ProcessDefinitionCreateResponse, error)
 	ProcessDefinitionGet(context.Context, *ProcessDefinitionRequest) (*ProcessDefinitionResponse, error)
-	// Task
 	TaskCreate(context.Context, *TaskCreateRequest) (*TaskCreateResponse, error)
 	TaskGet(context.Context, *TaskRequest) (*TaskResponse, error)
 	TaskDelete(context.Context, *TaskDeleteRequest) (*TaskDeleteResponse, error)
