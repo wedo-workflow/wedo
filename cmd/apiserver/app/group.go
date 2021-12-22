f0b28d854888759fe5a7e47547225eb97a20c44a
Jony4 <im@jony4.pro>
2021-12-22 14:55:20 +0800

    add group methods

---
+++ cmd/apiserver/app/group.go
@@ -0,0 +1,31 @@
+package app
+
+import (
+	"context"
+	wedo "github.com/wedo-workflow/wedo/api"
+)
+
+func (s *APIServer) GroupCreate(ctx context.Context, request *wedo.GroupCreateRequest) (*wedo.GroupCreateResponse, error) {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (s *APIServer) GroupGet(ctx context.Context, request *wedo.GroupRequest) (*wedo.GroupResponse, error) {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (s *APIServer) GroupDelete(ctx context.Context, request *wedo.GroupDeleteRequest) (*wedo.GroupDeleteResponse, error) {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (s *APIServer) GroupList(ctx context.Context, request *wedo.GroupListRequest) (*wedo.GroupListResponse, error) {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (s *APIServer) GroupUpdate(ctx context.Context, request *wedo.GroupUpdateRequest) (*wedo.GroupUpdateResponse, error) {
+	//TODO implement me
+	panic("implement me")
+}
